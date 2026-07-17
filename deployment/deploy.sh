3b72e2d89d2a8447b3e1479d89a75ea6ab4bce06
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-17 16:44:14 +0600

    fix: remove redundant git pull after reset --hard

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,41 +5,33 @@ echo "Starting deployment..."
 
 cd /home/ubuntu/devops-assessment || exit 1
 
+echo "🔄 Syncing with GitHub..."
 git fetch --all
 git reset --hard origin/master
 
-echo "Pulling latest code..."
-git pull origin master
-
-
-echo "Generating environment variables..."
+echo "⚙️ Generating environment variables..."
 cat <<EOF > .env
 COMPOSE_PROJECT_NAME=devops-assessment
 APP_ENV=production
 APP_VERSION=1.0.0
-
 NGINX_PORT=80
 NGINX_HOST=localhost
-
 SITE1_PATH=/site1
 SITE2_PATH=/site2
-
-DOCKER_NETWORK=devops-assesment
 RESTART_POLICY=unless-stopped
-
 HEALTHCHECK_INTERVAL=30s
 HEALTHCHECK_TIMEOUT=3s
 HEALTHCHECK_RETRIES=3
 EOF
 
-echo "Stopping old containers..."
+echo "🛑 Stopping old containers..."
 docker compose down
 
-echo "Building and starting containers..."
+echo "🏗️ Building and starting containers..."
 docker compose up -d --build
 
-echo "Cleaning up unused Docker images..."
+echo "🧹 Cleaning up unused Docker images..."
 docker image prune -f
 
-echo "Deployment successful!"
+echo "✅ Deployment successful!"
 docker compose ps
